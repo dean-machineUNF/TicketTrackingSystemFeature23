a38e2e906e11452c42cb90e16498d64884e0e9d5
dean-machineUNF <if.deanrice.istrue@gmail.com>
2019-04-29 17:15:15 -0400

    finished initial setup for MVC of Ticket

---
+++ TicketTrackingSystem/Controllers/TicketController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TicketTrackingSystem.Controllers
+{
+    public class TicketController : Controller
+    {
+        // GET: Ticket
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: OpenTickets
+        public ActionResult OpenTickets()
+        {
+            return View();
+        }
+
+        // GET: ClosedTickets
+        public ActionResult ClosedTickets()
+        {
+            return View();
+        }
+
+        // GET: EditTicket
+        public ActionResult EditTicket()
+        {
+            return View();
+        }
+
+        // GET: CreateTicket
+        public ActionResult CreateTicket()
+        {
+            return View();
+        }
+
+        // GET: CloseTicket
+        public ActionResult CloseTicket()
+        {
+            return View();
+        }
+    }
+}
---
+++ TicketTrackingSystem/Models/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketTrackingSystem.Models
+{
+    public class Ticket
+    {
+
+        public int TicketID { get; set; }
+        public string TicketDescription { get; set; }
+        public string TicketComment { get; set; }
+        public string TicketSeverity { get; set; }
+        public DateTime TicketDateCreated { get; set; }
+        public Boolean TicketIsResolved { get; set; }
+
+
+    }
+}
